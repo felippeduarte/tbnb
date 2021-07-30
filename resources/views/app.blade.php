a16936073e52d281c5104c72a8b8e527229186d1
felippeduarte <felippeduarte@gmail.com>
2021-07-30 03:46:23 -0300

    fixing assets for heroku

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>TBNB</title>
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" href="{{env('APP_ENV') == 'production' ? secure_asset('css/app.css') : asset('css/app.css')}}">
     </head>
     <body>
         <div id="app">
             <home></home>
         </div>
-        <script src="{{asset('js/app.js')}}"></script>
+        <script src="{{env('APP_ENV') == 'production' ? secure_asset('js/app.js') : asset('js/app.js')}}"></script>
     </body>
 </html>
